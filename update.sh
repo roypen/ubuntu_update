fd2d7f9900e55cc4d2fba856e3ba051abbf5fedf
Lukas Piekarski <43967723+roypen@users.noreply.github.com>
2020-08-08 00:01:15 +0200

    Update update.sh
    Added conditional statement for apt upgrade.

--- update.sh
+++ update.sh
@@ -3,18 +3,23 @@
 COLOUR='\033[0;32m'
 NC='\033[0m'
 
-echo -e "${COLOUR}\n Aktualizacja w toku...${NC}\n"
+echo -e "${COLOUR}\n Checking for system updates...${NC}\n"
 
-sudo apt update & sudo apt upgrade
+if ! sudo apt update | grep 'All packages are up to date';
+then
+    echo -e "${COLOUR}\n Starting apt update...${NC}\n"
+    sudo apt upgrade
+fi
+
 
 sleep 1
 
-echo -e "${COLOUR}\n Aktualizuję snap...${NC}\n"
+echo -e "${COLOUR}\n Starting snap update...${NC}\n"
 sudo snap refresh
 
 sleep 1
 
-echo -e "${COLOUR}\n Aktualizuję flatpak...${NC}\n"
+echo -e "${COLOUR}\n Starting flatpak update...${NC}\n"
 flatpak update
 
 
